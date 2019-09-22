6a875f5b2447a0d0e82b3389a8669f4097dfa7fa
Tracy Ogutu <tracyogutu@gmail.com>
2019-09-22 23:33:54 +0300

    add bootstrap styling

---
+++ README.md
@@ -0,0 +1,78 @@
+## PROJECT NAME
+# AKAN NAME WEB APPLICATION
+
+## AUTHOR NAME
+# TRACY OGUTU
+
+## PROJECT DESCRIPTION
+This project is a web application that provides the user with an Akan Name. It contains:
+ 
+ * _A brief history of the origin of Akan names_
+ * _A chart of the Akan Names_
+ * _A form that generates the Akan Name_
+
+ 
+### PROJECT SET UP INSTRUCTIONS
+* _INSTALLATION_
+
+1.Click on the github link and clone the repository
+
+2.Save the repository in a directory of choice
+
+3.Click the link https://atom.io/ to download atom
+
+4.Choose the download.deb option
+
+5.Open the downloaded file
+
+6.Click install 
+
+7.Open the terminal
+
+8.Navigate to the local repository where the project was cloned into
+
+9.Type the command atom . to launch atom 
+
+10.The code will then be displayed 
+
+* _USER INTERACTION_
+
+1.Scroll to the bottom of the website
+
+2.Fill in the form
+
+* _Do not leave blank input fields_
+
+* _Make valid inputs :_
+     * _Month entries should be between 1 and 12_
+     * _Day entry should be between 1 and 31_
+
+3.Click the submit button to generate Akan Name
+
+
+### BEHAVIOUR DRIVEN DEVELOPMENT(BDD)
+The web application allows a user to input their birthday through a form. It then calculates the day of the week they were born.Lastly it outputs their Akan name based on the day and gender.
+
+### TECHNOLOGIES USED
+_LANGUAGES_
+
+* _Html_
+
+* _CSS_
+
+* _JavaScript_
+
+* _Markdown_
+
+_FRAMEWORK_
+
+* _Bootstrap_
+
+### CONTACT INFORMATION
+
+Email: tracyogutu@gmail.com
+
+Phone:+254 719 614 987
+
+### LINK TO LIVE SITE ON GITHUB PAGES
+https://tracyogutu.github.io/AkanName
--- akaname.js
+++ akaname.js
@@ -5,47 +5,64 @@ function userInfo()
     var mm=parseInt(document.getElementById("mm").value);
     var dd=parseInt(document.getElementById("dd").value);
 
-    //alert("Your birthday is" +" "+ dd +" " + mm +" " + cc + yy);
-
+ 
+    if (mm = "" || mm<1 || mm>12) 
+    { 
+       alert("Please enter a Valid month ");
+       return false;
+    }
+ 
+    if (dd = "" || dd<1 || dd>31) 
+    { 
+       alert("Please enter a Valid day ");
+       return false;
+    }
+ 
+    
     var day = parseInt( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) %7;
 
-   
-    alert("The day of the week you were born is:" + " "+ day);
-    
     
     var gender=document.querySelector('input[name="gender"]:checked').value;
    
     
     if (gender==1)           
     {
+
         alert("You are female");
 
         if (day===0)
         {
+            alert("You were born on a Sunday")
             alert("Your Akan name is Akosua");
         }
         else if (day===1)
         {
+            alert("You were born on a Monday")
             alert("Your Akan name is Adwoa");
         }
         else if (day===2)
         {
+            alert("You were born on a Tuesday")
             alert("Your Akan name is Abenaa");
         }
         else if (day===3)
         {
+            alert("You were born on a Wednesday")
             alert("Your Akan name is Akua");
         }
         else if (day===4)
         {
+            alert("You were born on a Thursday")
             alert("Your Akan name is Yaa");
         }
         else if (day===5)
         {
+            alert("You were born on a Friday")
             alert("Your Akan name is Afua");
         }
         else if(day===6)
         {
+            alert("You were born on a Saturday")
             alert("Your Akan name is Ama");
         }
         else
@@ -60,30 +77,37 @@ function userInfo()
 
         if (day===0)
         {
+            alert("You were born on a Sunday")
             alert("Your Akan name is Kwasi");
         }
         else if (day===1)
         {
+            alert("You were born on a Monday")
             alert("Your Akan name is Kwadwo");
         }
         else if (day===2)
         {
+            alert("You were born on a Tuesday")
             alert("Your Akan name is Kwabena");
         }
         else if (day===3)
         {
+            alert("You were born on a Wednesday")
             alert("Your Akan name is Kwaku");
         }
         else if (day===4)
         {
+            alert("You were born on a Thursday")
             alert("Your Akan name is Yaw");
         }
         else if (day===5)
         {
+            alert("You were born on a Friday")
             alert("Your Akan name is Kofi");
         }
         else if (day===6)
          {
+            alert("You were born on a Saturday")
             alert("Your Akan name is Kwame");
         }
         else 
@@ -91,8 +115,7 @@ function userInfo()
             alert("Please try again. Input valid data");
         }
     }
+
+ }
    
-}
-
-
-
+ 
